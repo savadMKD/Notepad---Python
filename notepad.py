876a86ed34a696cacceee8a3e6a125b94c57db33
muhammed savad <savadmoorkkanad@gmail.com>
2021-03-22 16:52:59 +0530

    Setup Basic Window

--- notepad.py
+++ notepad.py
@@ -2,3 +2,17 @@ from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
 import os
+
+
+class Notepad:
+    window = Tk()
+    window.wm_iconbitmap("favicon.ico")
+    window.title("Untiled - Notes_App")
+    window.geometry("700x400")
+
+    def runApp(self):
+        self.window.mainloop()
+
+
+notesApp = Notepad()
+notesApp.runApp()
